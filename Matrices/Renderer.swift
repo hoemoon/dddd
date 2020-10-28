e99e077103abe0a6867422f06385e4944bb3dad8
hoemoon <hoemoon.song@gmail.com>
2020-10-28 21:26:23 +0900

    remove unnecessary code

--- Matrices/Renderer.swift
+++ Matrices/Renderer.swift
@@ -52,13 +52,6 @@ class Renderer: NSObject {
     Renderer.commandQueue = commandQueue
     metalView.device = device
     
-    // Add the train mesh
-    // Refer to the code in Chapter2.playground
-    let allocator = MTKMeshBufferAllocator(device: device)
-    guard let assetURL = Bundle.main.url(forResource: "train",
-                                         withExtension: "obj") else {
-                                          fatalError()
-    }
     let vertexDescriptor = MTLVertexDescriptor()
     vertexDescriptor.attributes[0].format = .float3
     vertexDescriptor.attributes[0].offset = 0
@@ -69,17 +62,6 @@ class Renderer: NSObject {
       MTKModelIOVertexDescriptorFromMetal(vertexDescriptor)
     (meshDescriptor.attributes[0] as! MDLVertexAttribute).name = MDLVertexAttributePosition
     
-    let asset = MDLAsset(url: assetURL,
-                         vertexDescriptor: meshDescriptor,
-                         bufferAllocator: allocator)
-    let mdlMesh = asset.childObjects(of: MDLMesh.self).first as! MDLMesh
-    do {
-      mesh = try MTKMesh(mesh: mdlMesh, device: device)
-    } catch let error {
-      print(error.localizedDescription)
-    }
-    vertexBuffer = mesh.vertexBuffers[0].buffer
-
     let library = device.makeDefaultLibrary()
     let vertexFunction = library?.makeFunction(name: "vertex_main")
     let fragmentFunction = library?.makeFunction(name: "fragment_main")
@@ -87,8 +69,6 @@ class Renderer: NSObject {
     let pipelineDescriptor = MTLRenderPipelineDescriptor()
     pipelineDescriptor.vertexFunction = vertexFunction
     pipelineDescriptor.fragmentFunction = fragmentFunction
-    pipelineDescriptor.vertexDescriptor =
-      MTKMetalVertexDescriptorFromModelIO(mdlMesh.vertexDescriptor)
     pipelineDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
     do {
       pipelineState = try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
@@ -97,14 +77,13 @@ class Renderer: NSObject {
     }
     
     super.init()
-    metalView.clearColor = MTLClearColor(red: 1.0, green: 1.0,
-                                         blue: 0.8, alpha: 1.0)
+    metalView.clearColor = MTLClearColor(
+			red: 1.0,
+			green: 1.0,
+			blue: 0.8,
+			alpha: 1.0
+		)
     metalView.delegate = self
-    
-    let translation = float4x4(translation: [0, 0.3, 0])
-    let rotation = float4x4(rotation: [0, Float(45).degreesToRadians, 0])
-    uniforms.modelMatrix = translation * rotation
-    uniforms.viewMatrix = float4x4(translation: [0.8, 0, 0]).inverse
     mtkView(metalView, drawableSizeWillChange: metalView.bounds.size)
   }
 	
@@ -130,13 +109,6 @@ class Renderer: NSObject {
 
 extension Renderer: MTKViewDelegate {
   func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-    let aspect = Float(view.bounds.width) / Float(view.bounds.height)
-    let projectionMatrix =
-      float4x4(projectionFov: Float(70).degreesToRadians,
-               near: 0.001,
-               far: 100,
-               aspect: aspect)
-    uniforms.projectionMatrix = projectionMatrix
   }
   
   func draw(in view: MTKView) {
